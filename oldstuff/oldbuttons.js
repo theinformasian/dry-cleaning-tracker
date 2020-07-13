5dc40dbf332419e4f0d3a6b7ff12e94c6e7a5ef8
Willa Cai <cai.willa@gmail.com>
2020-07-12 20:40:48 -0500

    removed oldstuff

--- oldstuff/oldbuttons.js
+++
@@ -1,9 +0,0 @@
-function increment(ele){
-  var counterName = ele.parentNode.id +'-counter';
-  document.getElementById(counterName).stepUp(1);
-}
-
-function decrement(ele){
-  var counterName = ele.parentNode.id +'-counter';
-  document.getElementById(counterName).stepDown(1);
-}
